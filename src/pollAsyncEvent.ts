d03ba027121349f78417bdc3f6687c8ddec3b247
Ben <br@massa.net>
2023-08-30 17:27:15 +0200

    test

--- src/pollAsyncEvent.ts
+++ src/pollAsyncEvent.ts
@@ -1,34 +1,19 @@
-import {
-  EventPoller,
-  Client,
-  INodeStatus,
-  IEventFilter,
-  ON_MASSA_EVENT_DATA,
-  ON_MASSA_EVENT_ERROR,
-} from "@massalabs/massa-web3";
-import { IEvent } from "@massalabs/web3-utils";
-
-interface IEventPollerResult {
-  isError: boolean;
-  eventPoller: EventPoller;
-  events: IEvent[];
-}
-const MASSA_EXEC_ERROR = "massa_execution_error";
+import { Client, EventPoller, IEvent, IEventFilter, INodeStatus, ON_MASSA_EVENT_DATA, ON_MASSA_EVENT_ERROR } from "@massalabs/massa-web3";
+import { IEventPollerResult } from "./test";
+const MASSA_EXEC_ERROR = 'massa_execution_error';
 
 const pollAsyncEvents = async (
   web3Client: Client,
-  opId: string
+  opId: string,
 ): Promise<IEventPollerResult> => {
   // determine the last slot
   let nodeStatusInfo: INodeStatus | null | undefined = await web3Client
     .publicApi()
     .getNodeStatus();
 
-  const start = nodeStatusInfo.last_slot;
-  start.period = start.period - 1000;
   // set the events filter
   const eventsFilter = {
-    start: start,
+    start: (nodeStatusInfo as INodeStatus).last_slot,
     end: null,
     original_caller_address: null,
     original_operation_id: opId,
@@ -39,14 +24,14 @@ const pollAsyncEvents = async (
   const eventPoller = EventPoller.startEventsPolling(
     eventsFilter,
     1000,
-    web3Client
+    web3Client,
   );
 
   return new Promise((resolve, reject) => {
     eventPoller.on(ON_MASSA_EVENT_DATA, (events: Array<IEvent>) => {
-      console.log("Event Data Received:", events);
+      console.log('Event Data Received:', events);
       let errorEvents: IEvent[] = events.filter((e) =>
-        e.data.includes(MASSA_EXEC_ERROR)
+        e.data.includes(MASSA_EXEC_ERROR),
       );
       if (errorEvents.length > 0) {
         return resolve({
@@ -56,18 +41,18 @@ const pollAsyncEvents = async (
         } as IEventPollerResult);
       }
 
-      if (events.length) {
+      if (events.length > 0) {
         return resolve({
           isError: false,
           eventPoller,
           events,
         } as IEventPollerResult);
       } else {
-        console.log("No events have been emitted during deployment");
+        console.log('No events have been emitted during deployment');
       }
     });
     eventPoller.on(ON_MASSA_EVENT_ERROR, (error: Error) => {
-      console.log("Event Data Error:", error);
+      console.log('Event Data Error:', error);
       return reject(error);
     });
   });
--- src/test.ts
+++ src/test.ts
@@ -1,9 +1,14 @@
-import { WalletClient, ClientFactory, DefaultProviderUrls } from "@massalabs/massa-web3";
+import { WalletClient, ClientFactory, DefaultProviderUrls, utils, EventPoller, IEvent, fromMAS } from "@massalabs/massa-web3";
 import pollAsyncEvents from "./pollAsyncEvent";
 import { readFileSync } from "fs";
 import path from "path";
 
 
+export interface IEventPollerResult {
+    isError: boolean;
+    eventPoller: EventPoller;
+    events: IEvent[];
+  }
 async function executeContract() {
     const account = await WalletClient.getAccountFromSecretKey(
       "S1JiEGozApGaUR8GzJ9CgDnw84iVVHhviXWkbt1MoXMPFqAiBC7"
@@ -14,17 +19,34 @@ async function executeContract() {
       account // base account
     );
 
-    console.log(client);
-    console.log(readFileSync(path.join(__dirname, 'build', 'test.wasm')));
-    const bytes = new Uint8Array(readFileSync(path.join(__dirname, 'build', 'test.wasm')));
+
     const opId = await client.smartContracts().deploySmartContract({
-      fee: BigInt(0),
-      maxGas: BigInt(100000000000000000),
-      maxCoins: BigInt(100000000000000000),
-      contractDataBinary: bytes,
+      fee: fromMAS(0.1),
+      maxGas: BigInt(4_200_000_000),
+      maxCoins: fromMAS(0.5),
+      contractDataBinary: readFileSync(path.join(__dirname, 'build', 'test.wasm')),
     });
     console.log(opId);
-    pollAsyncEvents(client, opId);
+    
+  // async poll events in the background for the given opId
+  const { isError, eventPoller, events }: IEventPollerResult =
+    await utils.time.withTimeoutRejection<IEventPollerResult>(
+      pollAsyncEvents(client, opId),
+      20000,
+    );
+
+  console.log(isError, eventPoller, events);
+
+  // stop polling
+  eventPoller.stopPolling();
+
+  // if errors, don't await finalization
+  if (isError) {
+    console.log(events);
+    throw new Error(
+      `Massa Deployment Error: ${JSON.stringify(events, null, 4)}`,
+    );
+  }
   }
 
   executeContract();
